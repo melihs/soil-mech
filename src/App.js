3696861cb52da0ed230ede189a09b382c43609a1
melih şahin <melihsahin24@gmail.com>
2020-09-18 18:40:13 +0300

    Add menu component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import MenuBar from './components/MenuBar';
+
 import './assets/App.css';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <>
+    <Row>
+      <Col span={24}><MenuBar/></Col>
+    </Row>
+    <Row>
+      <Col span={12}>col-12</Col>
+      <Col span={12}>col-12</Col>
+    </Row>
+    <Row>
+      <Col span={12}>col-12</Col>
+      <Col span={12}>col-12</Col>
+    </Row>
+  </>
   );
 }
 
---
+++ src/components/MenuBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Menu } from 'antd';
+import { CaretDownOutlined, QuestionOutlined, DownloadOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+
+// import 'antd/dist/antd.css';
+
+
+export default function MenuBar() {
+    return (
+        <Menu mode="horizontal">   
+            <SubMenu key="SubMenu" icon={< CaretDownOutlined />} title="Dosya">
+                <Menu.Item key="setting:1" icon={<DownloadOutlined />}>
+                    Dışarı Aktar
+                </Menu.Item>
+            </SubMenu>
+            <Menu.Item key="help" icon={ <QuestionOutlined />}>
+                Yardım                    
+            </Menu.Item> 
+        </Menu>
+    )
+}
