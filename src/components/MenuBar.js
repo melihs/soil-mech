489533589621d2fc84aaf18cbc82e74185d6d862
melih şahin <melihsahin24@gmail.com>
2020-09-24 23:22:19 +0300

    Add export report file

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,23 +1,36 @@
 import React from 'react'
-import { Menu } from 'antd';
-import { CaretDownOutlined, QuestionOutlined, DownloadOutlined } from '@ant-design/icons';
+import {Menu} from 'antd';
+import {CaretDownOutlined, QuestionOutlined, DownloadOutlined} from '@ant-design/icons';
+import * as XLSX from 'xlsx';
 
-const { SubMenu } = Menu;
+const {SubMenu} = Menu;
 
 // import 'antd/dist/antd.css';
 
+export default function MenuBar(props) {
+	let formData = props.data;
 
-export default function MenuBar() {
-    return (
-        <Menu mode="horizontal">   
-            <SubMenu key="SubMenu" icon={< CaretDownOutlined />} title="Dosya">
-                <Menu.Item key="export" icon={<DownloadOutlined />}>
-                    Dışarı Aktar
-                </Menu.Item>
-            </SubMenu>
-            <Menu.Item key="help" icon={ <QuestionOutlined />}>
-                Yardım                    
-            </Menu.Item> 
-        </Menu>
-    )
+	const exportToCSV = () => {
+		const ws = XLSX.utils.json_to_sheet(formData);
+		const fileName = 'report.xlsx';
+		const wb = XLSX.utils.book_new();
+
+		XLSX.utils.book_append_sheet(wb, ws, 'report');
+		XLSX.writeFile(wb, fileName);
+	}
+	return (
+		<Menu mode="horizontal">
+			<SubMenu key="SubMenu" icon={< CaretDownOutlined/>} title="Dosya">
+				<Menu.Item
+					key="export"
+					icon={<DownloadOutlined/>}
+					onClick={exportToCSV}>
+					Dışarı Aktar
+				</Menu.Item>
+			</SubMenu>
+			<Menu.Item key="help" icon={<QuestionOutlined/>}>
+				Yardım
+			</Menu.Item>
+		</Menu>
+	)
 }
